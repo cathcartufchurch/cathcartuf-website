be7b50688f3afd62d10791b1041aa2f3f36f0095
Cathcart UF Church <tech@cathcartuf.org.uk>
2026-06-06 23:05:57 +0100

    Test: Simple Hello World function

--- api/contact/index.js
+++ api/contact/index.js
@@ -1,7 +1,6 @@
 module.exports = async function (context, req) {
   context.res = {
     status: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Debug: Function is working" })
+    body: "Hello World"
   };
 };
