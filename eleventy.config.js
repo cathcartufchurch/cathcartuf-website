9784f3997469001b02337c0929bd4d8f5dd3807d
Cathcart UF Church <tech@cathcartuf.org.uk>
2026-06-10 08:47:45 +0100

    Fix: Passthrough HTML files and disable HTML template engine

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,12 +2,14 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("api");
+    eleventyConfig.addPassthroughCopy("*.html");
 
     // Ignore documentation files
     eleventyConfig.ignores.add("AZURE-LESSONS-LEARNED.md");
     eleventyConfig.ignores.add("README.md");
 
     return {
+        htmlTemplateEngine: false,
         dir: {
             input: ".",
             output: "_site",
