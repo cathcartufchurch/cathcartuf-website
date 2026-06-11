0506706ae745493b01bd938c45ae95ba2d2c0687
Cathcart UF Church <tech@cathcartuf.org.uk>
2026-06-11 10:37:17 +0100

    Migrate site to Eleventy with directory structure and base template

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,14 +2,16 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("api");
-    eleventyConfig.addPassthroughCopy("*.html");
 
-    // Ignore documentation files
+    // Ignore documentation and backup files
     eleventyConfig.ignores.add("AZURE-LESSONS-LEARNED.md");
     eleventyConfig.ignores.add("README.md");
+    eleventyConfig.ignores.add("_backups/**");
 
     return {
         htmlTemplateEngine: false,
+        markdownTemplateEngine: "njk",
+        templateFormats: ["html", "njk", "md"],
         dir: {
             input: ".",
             output: "_site",
